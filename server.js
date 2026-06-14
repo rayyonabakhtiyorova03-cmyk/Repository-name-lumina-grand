db603c2c4db32be9489f6f76f75bf4380f88dc84
rayyonabakhtiyorova03-cmyk <rayyonabakhtiyorova03@gmail.com>
2026-06-14 22:40:38 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,101 +1,42 @@
-const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname)));
 
-// =====================
-// BOOKINGS
-// =====================
+// frontend files
+app.use(express.static(path.resolve(__dirname)));
 
-function offsetDate(days) {
-    const d = new Date();
-    d.setDate(d.getDate() + days);
-    return d.toISOString().slice(0, 10);
-}
-
-let bookings = [
-    {
-        id: "LG-8821",
-        name: "John Doe",
-        room: "deluxe-ocean",
-        checkIn: offsetDate(-1),
-        checkOut: offsetDate(3),
-        nights: 4,
-        total: 1800,
-        status: "Confirmed"
-    }
-];
-
-// GET
-app.get("/bookings", (req, res) => {
-    res.json(bookings);
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
-// POST
-app.post("/bookings", (req, res) => {
-    const booking = req.body;
-    bookings.push(booking);
-    res.json({ success: true, booking });
+app.get("/test", (req, res) => {
+    res.send("OK SERVER WORKING");
 });
-
-// PUT
-app.put("/bookings/:id", (req, res) => {
-    const booking = bookings.find(b => b.id === req.params.id);
-    if (!booking) return res.status(404).json({ error: "Not found" });
-
-    Object.assign(booking, req.body);
-    res.json({ success: true, booking });
-});
-
-// DELETE
-app.delete("/bookings/:id", (req, res) => {
-    bookings = bookings.filter(b => b.id !== req.params.id);
-    res.json({ success: true });
-});
-
-// =====================
-// USERS
-// =====================
 
 let users = [];
 
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
     users.push(req.body);
     res.json({ success: true });
 });
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
     const user = users.find(
         u => u.email === req.body.email &&
              u.password === req.body.password
     );
 
     if (!user) {
-        return res.status(401).json({
-            success: false,
-            message: 'Invalid credentials'
-        });
+        return res.status(401).json({ success: false });
     }
 
     res.json({ success: true, user });
 });
-
-// =====================
-// FRONTEND ROUTE
-// =====================
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// =====================
-// START SERVER
-// =====================
 
 const PORT = process.env.PORT || 3000;
 
